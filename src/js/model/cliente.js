0888452f7c2da8613f2c17759d9108ca85a43fae
AgUsBF <agustinbeceyro@gmail.com>
2025-10-05 20:06:50 -0300

    feat: agregar clase Client

---
+++ src/js/model/cliente.js
@@ -0,0 +1,60 @@
+// CLASE CLIENTE
+
+export class Cliente {
+
+    // Campos privados
+    #id;
+    #nombreCompleto;
+    #email;
+    #direccion;
+    #telefono;
+
+    // Constructor
+    constructor(id, nombreCompleto = '', email = '', direccion = '', telefono = '') {
+        this.#id = id;
+        this.#nombreCompleto = String(nombreCompleto);
+        this.#email = String(email);
+        this.#direccion = String(direccion);
+        this.#telefono = String(telefono);
+    }
+
+    // Getters (métodos)
+    getId() {
+        return this.#id;
+    }
+    getNombreCompleto() {
+        return this.#nombreCompleto;
+    }
+    getEmail() {
+        return this.#email;
+    }
+    getDireccion() {
+        return this.#direccion;
+    }
+    getTelefono() {
+        return this.#telefono;
+    }
+
+    // Setters (métodos)
+    setNombreCompleto(nombreCompleto) {
+        this.#nombreCompleto = String(nombreCompleto);
+    }
+    setEmail(email) {
+        this.#email = String(email);
+    }
+    setDireccion(direccion) {
+        this.#direccion = String(direccion);
+    }
+    setTelefono(telefono) {
+        this.#telefono = String(telefono);
+    }
+
+    // Mostrar información breve
+    mostrarInfo() {
+        return `Cliente ID ${this.#id}: ${this.#nombreCompleto} - ${this.#email} - Tel: ${this.#telefono}`;
+    }
+
+    armarLabel() {
+        return `${this.#nombreCompleto} (${this.#telefono})`;
+    }
+}
